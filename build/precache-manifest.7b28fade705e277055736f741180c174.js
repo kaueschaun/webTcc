424f974a93bc538bf275602133200160271b804b
kaueschaun <kaueschaunsilva@gmail.com>
2021-12-14 14:08:17 -0300

    Adicionada pasta build

---
+++ build/precache-manifest.7b28fade705e277055736f741180c174.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bdd0449561cf01e9b72751098fb7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ba334bf778c77b7372",
+    "url": "/static/css/2.33195961.chunk.css"
+  },
+  {
+    "revision": "3a8650d551d53eda8caf",
+    "url": "/static/css/main.23ded0d5.chunk.css"
+  },
+  {
+    "revision": "08ba334bf778c77b7372",
+    "url": "/static/js/2.74c39420.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.74c39420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8650d551d53eda8caf",
+    "url": "/static/js/main.eb41844f.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "5dcc91fa8bb27679233c1ae75258b437",
+    "url": "/static/media/down-arrow.5dcc91fa.png"
+  },
+  {
+    "revision": "12d5f3bee93a63e5d40ad2e7ed3662e6",
+    "url": "/static/media/logo2.12d5f3be.png"
+  }
+]);
